f9037796a0c7082a00fee7fef4a82d041f11b8da
SidickSino <sidickabdoulayesino1@gmail.com>
2026-02-09 22:16:52 +0200

    Add Vercel deployment configuration, requirements, and update host settings and WSGI application for Vercel compatibility.

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wxb*#pr7$79fj@f(*x*a+-vmj##sd(g-_zners9d-xrtnuck_3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
