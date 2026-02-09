4e644ec2f850cec1271fbd2fed0b40efee937894
SidickSino <sidickabdoulayesino1@gmail.com>
2026-02-09 21:25:46 +0200

    Render the core app index view using a new `index.html` template instead of a direct HTTP response.

--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, World! This is the core app.")
+    return render(request, 'core/index.html')
